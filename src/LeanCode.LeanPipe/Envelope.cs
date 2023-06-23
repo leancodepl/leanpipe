48bd8f5f81e6a18757f345ab20652db8657e08a8
Michał M. Więcław <michal.wieclaw@leancode.pl>
2023-06-23 09:45:18 +0200

    Include fully qualified name in the envelope

--- src/LeanCode.LeanPipe/Envelope.cs
+++ src/LeanCode.LeanPipe/Envelope.cs
@@ -4,8 +4,8 @@ namespace LeanCode.LeanPipe;
 
 public class Envelope(ITopic topic, object notification)
 {
-    public string Topic { get; } = topic.GetType().Name;
-    public string Notification { get; } = notification.GetType().Name;
+    public string Topic { get; } = topic.GetType().FullName!;
+    public string Notification { get; } = notification.GetType().FullName!;
     public ITopic Subscription { get; } = topic;
     public object Payload { get; } = notification;
 }
