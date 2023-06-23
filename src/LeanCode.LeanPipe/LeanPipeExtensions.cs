3396c2c80f595e4cdc5333189f26cc6b802d1957
Michał M. Więcław <michal.wieclaw@leancode.pl>
2023-06-23 14:08:34 +0200

    Add simple extensions for registration

---
+++ src/LeanCode.LeanPipe/LeanPipeExtensions.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace LeanCode.LeanPipe.Extensions;
+
+public static class LeanPipeExtensions
+{
+    public static IServiceCollection AddLeanPipe(this IServiceCollection services)
+    {
+        services.AddSignalR();
+        services.TryAdd(ServiceDescriptor.Transient(typeof(IKeysFactory<>), typeof(DefaultKeysFactory<>)));
+        services.TryAdd(ServiceDescriptor.Transient(typeof(ILeanPipeContext<,>), typeof(LeanPipeContext<,>)));
+        return services;
+    }
+
+    public static IHubEndpointConventionBuilder MapLeanPipe(this IEndpointRouteBuilder endpoints, string pattern)
+    {
+        return endpoints.MapHub<LeanPipe>(pattern);
+    }
+    public static IHubEndpointConventionBuilder MapLeanPipe(this IEndpointRouteBuilder endpoints, string pattern, Action<HttpConnectionDispatcherOptions>? configureOptions)
+    {
+        return endpoints.MapHub<LeanPipe>(pattern, configureOptions);
+    }
+}
