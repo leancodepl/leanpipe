c4fdf1d27d582542285c5f2d9816e2d78c805b72
Michał M. Więcław <michal.wieclaw@leancode.pl>
2023-06-28 10:01:26 +0200

    Setup front end

--- src/LeanPipe.Example/src/index.ts
+++ src/LeanPipe.Example/src/index.ts
@@ -2,16 +2,28 @@ import * as signalR from "@microsoft/signalr";
 import "./css/main.css";
 
 const divMessages: HTMLDivElement = document.querySelector("#divMessages");
-const tbMessage: HTMLInputElement = document.querySelector("#tbMessage");
-const btnSend: HTMLButtonElement = document.querySelector("#btnSend");
+const tbTopic: HTMLInputElement = document.querySelector("#tbTopic");
+const tbAmount: HTMLInputElement = document.querySelector("#tbAmount");
+const btnSubscribe: HTMLButtonElement = document.querySelector("#btnSubscribe");
+const btnUnsubscribe: HTMLButtonElement =
+  document.querySelector("#btnUnsubscribe");
+const btnBid: HTMLButtonElement = document.querySelector("#btnBid");
+const btnBuy: HTMLButtonElement = document.querySelector("#btnBuy");
 const username = new Date().getTime();
 
-const connection = new signalR.HubConnectionBuilder().withUrl("/hub").build();
+const connection = new signalR.HubConnectionBuilder().withUrl("/pipe").build();
 
-connection.on("messageReceived", (username: string, message: string) => {
+interface Envelope {
+  topicType: string;
+  notificationType: string;
+  topic: object;
+  notification: object;
+}
+
+connection.on("notify", (message: Envelope) => {
   const m = document.createElement("div");
 
-  m.innerHTML = `<div class="message-author">${username}</div><div>${message}</div>`;
+  m.innerHTML = `<div>${JSON.stringify(message)}</div>`;
 
   divMessages.appendChild(m);
   divMessages.scrollTop = divMessages.scrollHeight;
@@ -19,16 +31,48 @@ connection.on("messageReceived", (username: string, message: string) => {
 
 connection.start().catch((err) => document.write(err));
 
-tbMessage.addEventListener("keyup", (e: KeyboardEvent) => {
-  if (e.key === "Enter") {
-    send();
-  }
+btnSubscribe.addEventListener("click", () => {
+  connection.send("SubscribeAsync", {
+    TopicType: "LeanPipe.Example.Contracts.Auction",
+    Topic: JSON.stringify({ AuctionId: tbTopic.value }),
+  });
+});
+btnUnsubscribe.addEventListener("click", () => {
+  connection.send("UnsubscribeAsync", {
+    TopicType: "LeanPipe.Example.Contracts.Auction",
+    Topic: JSON.stringify({ AuctionId: tbTopic.value }),
+  });
+});
+btnBid.addEventListener("click", (event) => {
+  fetch(normalize("/cqrs/command/LeanPipe.Example.Contracts.PlaceBid"), {
+    method: "POST",
+    body: JSON.stringify({
+      AuctionId: tbTopic.value,
+      Amount: Number(tbAmount.value),
+      UserId: username.toString(),
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+  event.preventDefault();
+});
+btnBuy.addEventListener("click", (event) => {
+  fetch(normalize("/cqrs/command/LeanPipe.Example.Contracts.Buy"), {
+    method: "POST",
+    body: JSON.stringify({
+      AuctionId: tbTopic.value,
+      userId: username.toString(),
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+  event.preventDefault();
 });
 
-btnSend.addEventListener("click", send);
-
-function send() {
-  connection
-    .send("newMessage", username, tbMessage.value)
-    .then(() => (tbMessage.value = ""));
+function normalize(uri) {
+  const aTag = window.document.createElement("a");
+  aTag.href = uri;
+  return aTag.href;
 }
