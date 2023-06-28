b33c86cedcd3b30692c66ab60310ba7a8aa4c9d9
Michał M. Więcław <michal.wieclaw@leancode.pl>
2023-06-28 16:58:23 +0200

    Remove default keys factory

--- src/LeanCode.LeanPipe/KesyFactory.cs
+++ src/LeanCode.LeanPipe/KesyFactory.cs
@@ -7,18 +7,3 @@ public interface IKeysFactory<in TTopic>
 {
     IEnumerable<string> ToKeys(TTopic topic);
 }
-
-public class DefaultKeysFactory<TTopic>
-    where TTopic : ITopic
-{
-    public virtual HashSet<string> ToKeys(TTopic topic)
-    {
-        return new HashSet<string>()
-        {
-            topic.ToString()
-                ?? throw new InvalidOperationException(
-                    "Topic must provide a valid string representation"
-                )
-        };
-    }
-}
