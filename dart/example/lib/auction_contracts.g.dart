b4b33fae90912919ec2325953eceb2a9e09a2c3e
shilangyu <xmarcinmarcin@gmail.com>
2023-09-25 16:44:19 +0200

    Add example

---
+++ dart/example/lib/auction_contracts.g.dart
@@ -0,0 +1,35 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auction_contracts.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Auction _$AuctionFromJson(Map<String, dynamic> json) => Auction(
+      json['AuctionId'] as String,
+      json['Authorized'] as bool,
+    );
+
+Map<String, dynamic> _$AuctionToJson(Auction instance) => <String, dynamic>{
+      'AuctionId': instance.auctionId,
+      'Authorized': instance.authorized,
+    };
+
+BidPlaced _$BidPlacedFromJson(Map<String, dynamic> json) => BidPlaced(
+      amount: json['Amount'] as int,
+      user: json['User'] as String,
+    );
+
+Map<String, dynamic> _$BidPlacedToJson(BidPlaced instance) => <String, dynamic>{
+      'Amount': instance.amount,
+      'User': instance.user,
+    };
+
+ItemSold _$ItemSoldFromJson(Map<String, dynamic> json) => ItemSold(
+      buyer: json['Buyer'] as String,
+    );
+
+Map<String, dynamic> _$ItemSoldToJson(ItemSold instance) => <String, dynamic>{
+      'Buyer': instance.buyer,
+    };
