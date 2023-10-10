1f97e09e108d0ea9ce86cba82f2a4a427639f0cd
Jakub Lange <jakublange@Jakubs-MacBook-Air.local>
2023-10-10 19:02:13 +0200

    Remove old files, generate new contracts

--- dart/example/lib/app.dart
+++ dart/example/lib/app.dart
@@ -1,18 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:http/http.dart';
+import 'package:leancode_contracts/leancode_contracts.dart';
 import 'package:leancode_pipe/leancode_pipe.dart';
-import 'package:leancode_pipe_example/screen_cubit.dart';
+import 'package:leancode_pipe_example/projects_screen/bloc/projects_cubit.dart';
+import 'package:leancode_pipe_example/projects_screen/projects_screen.dart';
 import 'package:provider/provider.dart';
 
-class LeancodePipeDraftApp extends StatelessWidget {
-  const LeancodePipeDraftApp({super.key});
+class App extends HookWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
+    return MaterialApp(
+      home: MultiProvider(
+        providers: [
+          Provider(
+            create: (context) => Cqrs(
+              Client(),
+              Uri.parse('https://exampleapp.test.lncd.pl/api/'),
+            ),
+          ),
+          Provider(
+            create: (context) => PipeClient(
+              pipeUrl: 'https://exampleapp.test.lncd.pl/leanpipe',
+              tokenFactory: () async => 'token',
+            ),
+          ),
+        ],
+        child: BlocProvider(
+          create: (context) =>
+              ProjectsCubit(cqrs: context.read())..fetchProjects(),
+          lazy: false,
+          child: HomeScreen(),
+        ),
+      ),
     );
   }
 }
@@ -22,132 +45,6 @@ class HomeScreen extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final bidController = useTextEditingController();
-
-    return Provider(
-      create: (context) => PipeClient(
-        pipeUrl: ScreenCubit.leanPipeExampleUrl,
-        tokenFactory: () async => 'token',
-      ),
-      dispose: (context, value) => value.dispose(),
-      child: BlocProvider(
-        create: (context) => ScreenCubit(context.read()),
-        child: Scaffold(
-          appBar: AppBar(title: const Text('Leancode Pipe Draft')),
-          body: BlocBuilder<ScreenCubit, DraftScreenState>(
-            builder: (context, state) => SafeArea(
-              minimum:
-                  const EdgeInsets.symmetric(horizontal: 16).copyWith(top: 16),
-              child: Stack(
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Expanded(
-                        child: SingleChildScrollView(
-                          child: Text(
-                            state.map(
-                              connected: (state) => state.data,
-                              disconnected: (state) =>
-                                  'Disconnected. Error: ${state.error}. Connecting: ${state.connecting}',
-                            ),
-                            textAlign: TextAlign.start,
-                          ),
-                        ),
-                      ),
-                      const Divider(height: 32, thickness: 2),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: TextFormField(
-                              controller: bidController,
-                              keyboardType: TextInputType.number,
-                              inputFormatters: [
-                                FilteringTextInputFormatter.digitsOnly,
-                              ],
-                            ),
-                          ),
-                          const SizedBox(width: 16),
-                          ElevatedButton(
-                            onPressed: () {
-                              final value = int.tryParse(bidController.text);
-                              if (value != null) {
-                                context.read<ScreenCubit>().placeBid(value);
-                              }
-                            },
-                            child: const Text('Place bid'),
-                          ),
-                          const SizedBox(width: 16),
-                          ElevatedButton(
-                            onPressed: context.read<ScreenCubit>().buy,
-                            child: const Text('Buy'),
-                          ),
-                        ],
-                      ),
-                      const Divider(thickness: 2, height: 24),
-                      Row(
-                        children: [
-                          const Text('Authorized:'),
-                          const SizedBox(width: 16),
-                          Checkbox(
-                            value: state.authorized,
-                            onChanged: (_) => context
-                                .read<ScreenCubit>()
-                                .switchAuthorization(),
-                          ),
-                        ],
-                      ),
-                      const Divider(thickness: 2, height: 24),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: OutlinedButton(
-                              onPressed: context.read<ScreenCubit>().subscribe,
-                              child: const Text('Subscribe'),
-                            ),
-                          ),
-                          const SizedBox(width: 12),
-                          Expanded(
-                            child: OutlinedButton(
-                              onPressed:
-                                  context.read<ScreenCubit>().unsubscribe,
-                              child: const Text('Unsubscribe'),
-                            ),
-                          ),
-                        ],
-                      ),
-                      const Divider(thickness: 2, height: 24),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: OutlinedButton(
-                              onPressed: context.read<ScreenCubit>().connect,
-                              child: const Text('Connect'),
-                            ),
-                          ),
-                          const SizedBox(width: 12),
-                          Expanded(
-                            child: OutlinedButton(
-                              onPressed: context.read<ScreenCubit>().disconnect,
-                              child: const Text('Disconnect'),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  if (state is DraftScreenDisconnected && state.connecting)
-                    const Positioned.fill(
-                      child: IgnorePointer(
-                        child: Center(child: CircularProgressIndicator()),
-                      ),
-                    ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
+    return ProjectsScreen();
   }
 }
--- dart/example/lib/main.dart
+++ dart/example/lib/main.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 import 'app.dart';
 
 void main() {
-  runApp(const LeancodePipeDraftApp());
+  runApp(const App());
 }
