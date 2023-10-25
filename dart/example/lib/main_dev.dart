80579958eee944202a004f2a4cf701d02d81f8b6
Jakub Lange <jakublange@Jakubs-MacBook-Air.local>
2023-10-25 23:53:21 +0200

    Shrink AppConfig class

--- dart/example/lib/main_dev.dart
+++ dart/example/lib/main_dev.dart
@@ -3,15 +3,9 @@ import 'package:app/main_common.dart';
 
 void main() {
   final config = AppConfig(
-    // TODO: replace Bugfender key
-    bugfenderKey: '00l0bRl0scod4VVsOc24x60Xdl747Pqd',
-    // TODO: replace api uri
     apiUri: Uri.parse('https://exampleapp.test.lncd.pl/api/'),
-    // TODO: replace pipe uri
     pipeUri: Uri.parse('https://exampleapp.test.lncd.pl/leanpipe'),
-    configCatSdkKey: 'LqLbCBSqfUCF8J0xspJ_rw/VOk3Ffaud02lKDk9OuFeSA',
     kratosUri: Uri.parse('https://auth.exampleapp.test.lncd.pl'),
-    debugMode: true,
   );
 
   mainCommon(config);
