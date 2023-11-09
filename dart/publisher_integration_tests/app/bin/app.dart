a7d86bb2684768ec15183d6f29b11e5603e90f08
Jakub Lange <jakub.lange.gd@gmail.com>
2023-11-09 13:44:33 +0100

    Write the test

--- dart/publisher_integration_tests/app/bin/app.dart
+++ dart/publisher_integration_tests/app/bin/app.dart
@@ -1,35 +1,85 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:leancode_pipe/leancode_pipe/pipe_client.dart';
 import 'package:publisher_client_integration_tests/topic.dart';
 import 'package:test/test.dart';
+import 'package:http/http.dart' as http;
 
-final funnelDisabledEndpoint =
-    Uri.parse('http://testapp-funneldisabled-svc.default.svc.cluster.local/')
-        .resolve('leanpipe')
-        .toString();
-final funnelEnabledEndpoint = Uri.parse(
-        'localhost:8080/testapp-funnelenabled-svc.default.svc.cluster.local')
-    .resolve('leanpipe')
-    .toString();
+// final funnelDisabledEndpoint =
+//     Uri.parse('http://testapp-funneldisabled-svc.default.svc.cluster.local/')
+//         .resolve('leanpipe')
+//         .toString();
+// final funnelDisabledEndpoint = 'http://localhost:5151/leanpipe';
+// final funnelDisabledPostEndpoint = 'http://localhost:5151/publish';
+// final funnelEnabledEndpoint = Uri.parse(
+//         'localhost:8080/testapp-funnelenabled-svc.default.svc.cluster.local')
+//     .resolve('leanpipe')
+//     .toString();
 
 const testAccessToken = '1234';
 
+Future<void> _triggerNotification(Topic topic, String endpoint) {
+  final body = jsonEncode(topic.toJson());
+
+  return http.post(
+    Uri.parse(endpoint).resolve('publish'),
+    headers: {'Content-Type': 'application/json'},
+    body: body,
+  );
+}
+
 void main() {
   late PipeClient pipeClient;
+  late String endpoint;
 
-  setUpAll(
+  setUp(
     () {
+      const envVarName = 'TEST_ENDPOINT';
+      endpoint = switch (Platform.environment[envVarName]) {
+        final endpoint? => endpoint,
+        _ => throw StateError('Environment variable $envVarName is not set'),
+      };
+
       pipeClient = PipeClient(
-        pipeUrl: funnelDisabledEndpoint,
+        pipeUrl: Uri.parse(endpoint).resolve('leanpipe').toString(),
         tokenFactory: () async => testAccessToken,
       );
     },
   );
 
-  test('subscribe', () async {
+  test(
+      'Basic scenario: connect, subscribe, trigger notification, receive notification, unsubscribe, trigger notification, make sure nothing is received, cancel subscriptions, disconnect',
+      () async {
+    const waitTime = Duration(seconds: 1);
+    const topic = Topic(id: '1');
+
+    bool received = false;
+
     await pipeClient.connect();
 
-    await pipeClient.subscribe(Topic(id: '1'));
-  });
+    final subscription = await pipeClient.subscribe(topic);
 
-  print('test passed');
+    final handlerSubscription = subscription.listen((value) {
+      received = true;
+    });
+
+    await _triggerNotification(topic, endpoint);
+
+    await Future.delayed(waitTime);
+
+    expect(received, true);
+
+    await subscription.unsubscribe();
+    received = false;
+
+    await _triggerNotification(topic, endpoint);
+
+    await Future.delayed(waitTime);
+
+    expect(received, false);
+
+    await handlerSubscription.cancel();
+    await pipeClient.disconnect();
+  });
 }
--- dart/publisher_integration_tests/app/lib/topic.dart
+++ dart/publisher_integration_tests/app/lib/topic.dart
@@ -7,24 +7,38 @@ class Topic implements leanpipe.Topic<NotificationDTO> {
 
   @override
   NotificationDTO? castNotification(String tag, json) {
-    throw UnimplementedError();
+    return NotificationDTO(greeting: json['Greeting']);
   }
 
   @override
   leanpipe.Topic<NotificationDTO> fromJson(Map<String, dynamic> json) {
-    return Topic(id: json['topicId']);
+    return Topic(id: json['TopicId']);
   }
 
   @override
-  String getFullName() {
-    // TODO: implement getFullName
-    throw UnimplementedError();
-  }
+  String getFullName() => 'LeanCode.Pipe.ClientIntegrationTestsApp.Topic';
 
   @override
   Map<String, dynamic> toJson() {
-    return {'topicId': id};
+    return {'TopicId': id};
   }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Topic && runtimeType == other.runtimeType && id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
 }
 
-class NotificationDTO {}
+class NotificationDTO {
+  const NotificationDTO({required this.greeting});
+
+  factory NotificationDTO.fromJson(Map<String, dynamic> json) =>
+      NotificationDTO(greeting: json['Greeting']);
+
+  final String greeting;
+
+  Map<String, dynamic> toJson() => {'Greeting': greeting};
+}
